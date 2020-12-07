d35108d286d20b778f48fdee3af2c4dac7a60c43
Mike Shelby <mikegshelby@gmail.com>
2020-12-07 16:52:34 -0600

    test all methods associated with Employee constructor

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,11 +1,40 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-    const employee = new Employee('mike', 3, 'mikegshelby@gmail.com');
+    const employee = new Employee('mike', '3', 'mikegshelby@gmail.com');
 
     expect(employee.name).toEqual(expect.any(String));
-    expect(employee.id).toEqual(expect.any(Number));
+    expect(employee.id).toEqual(expect.any(String));
     expect(employee.email).toEqual(expect.any(String));
 
 
 });
+
+test("returns employee's name as a string", () => {
+    const employee = new Employee('mike');
+
+    expect(employee.getName()).toEqual(expect.any(String));
+});
+
+test("returns employee's id as a number", () => {
+    const employee = new Employee('mike', '3');
+
+    expect(employee.getID()).toEqual(expect.any(String));
+});
+
+test("returns employee's email address as a string", () => {
+    const employee = new Employee('mike', '3', 'mikegshelby@gmail.com');
+
+    expect(employee.getEmail()).toEqual(expect.any(String));
+});
+
+test("returns employee's role ('Employee')", () => {
+    const employee = new Employee();
+
+    expect(employee.getRole()).toEqual(expect.any(String));
+});
+
+
+
+
+
